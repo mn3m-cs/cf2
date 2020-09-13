ba629779d380ea74c692dc4e0eaa2ff47ef11d61
Mohamed Abdelmoneim <34791641+mn3m-cs@users.noreply.github.com>
2020-09-13 16:25:06 +0200

    clean admin

--- cof/admin.py
+++ cof/admin.py
@@ -4,11 +4,8 @@ from cof.models import Machine,Pod
 
 class MachineAdmin(admin.ModelAdmin):
     list_filter = ('product_type','water_line_compitable','model')
-
 admin.site.register(Machine,MachineAdmin)
-
 
 class PodAdmin(admin.ModelAdmin):
     list_filter = ('product_type','flavor','pack_size')
-
 admin.site.register(Pod,PodAdmin)
